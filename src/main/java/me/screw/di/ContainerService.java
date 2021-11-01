df0ec866910449aaf7cbbfa9bf2d711f36458148
seokkyu <tjrrb95@gmail.com>
2021-11-01 23:05:41 +0900

    feat(create_instance): 리플렉션을 사용하여 생성자 인자없이 객체 생성하기.

--- src/main/java/me/screw/di/ContainerService.java
+++ src/main/java/me/screw/di/ContainerService.java
@@ -1,9 +1,15 @@
 package me.screw.di;
 
+import java.lang.reflect.InvocationTargetException;
+
 public class ContainerService {
 
     public static <T> T getObject(Class<T> classType) {
-        return null;
+        try {
+            return classType.getConstructor(null).newInstance();
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            throw new RuntimeException();
+        }
     }
 
 }
