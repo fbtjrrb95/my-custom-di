e179d6d88b3242f219cdcc39b9bdec8b28e23093
seokkyu <tjrrb95@gmail.com>
2021-11-02 23:21:11 +0900

    feat(create_instance): field 객체 주입하기

--- src/main/java/me/screw/di/ContainerService.java
+++ src/main/java/me/screw/di/ContainerService.java
@@ -1,12 +1,26 @@
 package me.screw.di;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
 
 public class ContainerService {
 
     public static <T> T getObject(Class<T> classType) {
         try {
-            return classType.getConstructor(null).newInstance();
+            T instance = classType.getConstructor(null).newInstance();
+            Arrays.stream(classType.getDeclaredFields()).forEach(f -> {
+                Inject annotation = f.getAnnotation(Inject.class);
+                if (annotation != null) {
+                    try {
+                        Object fieldInstance = f.getType().getConstructor(null).newInstance();
+                        f.setAccessible(true);
+                        f.set(instance, fieldInstance);
+                    } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+                        throw new RuntimeException();
+                    }
+                }
+            });
+            return instance;
         } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
             throw new RuntimeException();
         }
